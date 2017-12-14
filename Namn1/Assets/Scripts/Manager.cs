1231b3b0c4c2d19a4af57e80ee9c09ad09c133a9
GoodGuyTobi <arvid.tapper@falun.ntig.se>
2017-12-14 02:50:41 +0100

    -Gjort en mapp för Unity och laddat upp 2 applikationer.
    -Här har jag skapat en mapp där framtida Unity-projekt kommer att hamna.
    
    -Jag har än så länge två projekt i denna mapp, den första kommer från lektionen då jag satt med Programmering 2 när de gick igenom Unity.
    
    -Andra applikationen gjordes när Marsus hade en grundläggande genomgång inom Unity.

---
+++ Namn1/Assets/Scripts/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(spanAlien());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator spanAlien()
+    {
+        yield return new WaitForSeconds(-1f);
+        Instantiate(prefab, transform);
+        StartCoroutine(spanAlien());
+    }
+}
---
+++ Namn1/Assets/Scripts/PlayCont.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCont : MonoBehaviour {
+
+    Rigidbody rb;
+
+    [SerializeField]
+    private float baseSpeed = 3.14f;
+    [SerializeField]
+    private float thrustForce = 3.14f;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveX = Input.GetAxis("Horizontal") * baseSpeed * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * baseSpeed * Time.deltaTime;
+
+        rb.MovePosition(new Vector3(moveX + transform.position.x, transform.position.y, moveZ + transform.position.z));
+
+        if(Input.GetAxis("Jump")>0)
+        {
+            rb.AddForce(new Vector3(0,thrustForce * 10,0));
+        }
+
+	}
+}
